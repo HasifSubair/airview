d597242f285b466098c213f28f645f81995f82cd
Hasif Subair <subairhasif@gmail.com>
2019-07-22 16:38:52 +0200

    Changed the method name from ingest_flight to ingest_data.

--- executor.py
+++ executor.py
@@ -18,13 +18,12 @@ class Executor:
         """
         return session.SparkSession.Builder().appName(app_name).master(master).getOrCreate()
 
-    def ingest_flight(self, *params):
+    def ingest_data(self, *params):
         DataIngester().ingest_data(input_path=params[0], input_format=params[1], output_path=params[2],
-                                              output_format=params[3], write_mode=params[4],
-                                              spark=self.get_spark_session("Ingest Flight Data"))
+                                   output_format=params[3], write_mode=params[4],
+                                   spark=self.get_spark_session("Ingest Flight Data"))
 
 
 if __name__ == "__main__":
     args = sys.argv
     getattr(Executor(), args[1])(*args[2:])
-    # Executor().execute_task(func)
