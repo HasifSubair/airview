43d8132281bc4fb5462ec0bcb9a0f9b8f7415d62
Hasif Subair <subairhasif@gmail.com>
2019-07-25 20:52:56 +0200

    Added pydocs.

--- executor.py
+++ executor.py
@@ -1,15 +1,24 @@
 import sys
-from extract import DataManager
-from transform import FlightDataManager, AircraftManager
+
 import pyspark.sql.session as session
+
+from data_manager import DataManager
+from transform import FlightManager, AircraftManager
 
 
 class Executor:
+    """
+    Executor class acts as the entry point to the program. You can execute the program by passing the method name as
+    the first argument and followed by the arguments for the method. For example, for data ingestion you can execute the
+    command in below format
+
+    executor.py ingest_data <INPUT_LOCATION> <INPUT_FORMAT> <OUTPUT_LOCATION> <OUTPUT_FORMAT>
+    """
 
     def get_spark_session(self, app_name, master='local[*]'):
         """
-        Function initiates a SparkSession instance. Master is defaulted to local. Can be extended to
-        property driven or removed to be executed with spark-submit.
+        Function initiates a SparkSession instance. Master is defaulted to local. Can be extended to property driven or
+        removed to be executed with spark-submit.
         :param app_name: str
                         Application name
         :param master: str
@@ -20,23 +29,62 @@ class Executor:
         return session.SparkSession.Builder().appName(app_name).master(master).getOrCreate()
 
     def show_schema(self, input_path, input_format, header=True):
+        """
+        Method prints the schema of the file in the input path. Method uses sparks internal schema inference to
+        determine the file schema. It reads a few sample records to determine the type of the record.
+        :param input_path: Path to the file whose schema is to determined.
+        :param input_format: File format of the input file.
+        :param header: For files with header information, doesn't have an effect on files without it.
+        :return: NoneType
+        """
         data_frame = self.get_spark_session(f'Showing schema for {input_path}').read.format(input_format).option(
             "header", header).load(f"{input_path}")
         data_frame.printSchema()
 
     def ingest_data(self, *params):
+        """
+        Method reads files from the based on the parameters and writes to the output location based on the parameters.
+        The method invokes DataManager to ingest the data by creating and passing a SparkSession instance along with the
+        input parameters.
+        :param params: Parameters for the ingesting the data.
+        :return: NoneType.
+        """
         DataManager().ingest_data(self.get_spark_session(f"Ingesting Data from {params[0]}"), *params)
 
     def clean_flight_data(self, *params):
-        FlightDataManager().clean_flight_data(self.get_spark_session(f"Flight data cleaning: {params[0]}"), *params)
+        """
+        Method reads the data from the input location in the parameter and cleans the flight data and stores it in the
+        output location specified in the parameters. Method invokes FlightManager class' clean flight data method with
+        an instance of SparkSession along with the input parameters.
+        :param params: Parameter for clean the flight data.
+        :return: NoneType.
+        """
+        FlightManager().clean_flight_data(self.get_spark_session(f"Flight data cleaning: {params[0]}"), *params)
 
     def clean_aircraft_data(self, *params):
+        """
+        Method reads the data from the input location in the parameter and cleans the aircraft data and stores it in the
+        output location specified in the parameters. Method invokes AircraftManager class' clean aircraft data method with
+        an instance of SparkSession along with the input parameters.
+        :param params: Parameter for clean the aircraft data.
+        :return: NoneType.
+        """
         AircraftManager().clean_aircraft_data(self.get_spark_session(f"Aircraft data cleaning: {params[0]}"), *params)
 
     def enhance_flight_data(self, *params):
-        FlightDataManager().enhance_flight_details(self.get_spark_session('Enhancing Flight details'), *params)
+        """
+        Method creates the analytical dataset by joining airline, airport and aircraft data with the flight dataset.
+        Method invoke FlightManager class' enhance flight details method with an instance of SparkSession anf input
+        parameters.
+        :param params: Parameter for enhance the flight dataset.
+        :return: NoneType.
+        """
+        FlightManager().enhance_flight_details(self.get_spark_session('Enhancing Flight details'), *params)
 
 
 if __name__ == "__main__":
+    # Getting the python arguments.
     args = sys.argv
+    # Method to be called in executor class is set as the first argument, rest of the arguments are passed to that
+    # method as tuples.
     getattr(Executor(), args[1])(*args[2:])
--- transform.py
+++ transform.py
@@ -1,10 +1,31 @@
-from extract import DataManager
+"""
+The script contains all the transformation to be performed on the input data.
+"""
+from data_manager import DataManager
 from pyspark.sql.functions import col, regexp_replace, concat, concat_ws, lpad, to_timestamp, unix_timestamp, \
     from_unixtime, to_date
 
 
-class FlightDataManager:
+class FlightManager:
+    """
+    Class contains all the transformation and data cleaning methods for flight data.
+    """
+
     def clean_flight_data(self, spark, path, format, output_path, output_format, write_mode='error'):
+        """
+        Method converts the time columns scheduled departure time(CRSDepTime), scheduled arrival time(CRSArrTime),
+        actual departure time(DepTime) and actual arrival time(ArrTime) to a timestamp columns. The original format for
+        these columns in 'HHmm' and is converted to 'yyyy-MM-dd HH:mm:ss' format, which is much more human readable. The
+        columns 'DayofMonth' and 'DayOfWeek' dropped as it is no longer required. The data frame is written to the
+        output folder in the specified format.
+        :param spark: SparkSession instance.
+        :param path: Path to the input folder.
+        :param format: Input file format.
+        :param output_path: Output file path
+        :param output_format: Output file format.
+        :param write_mode: File write mode defaulted to 'error'
+        :return: NoneType
+        """
         data_frame = DataManager().read_data(spark, path, format)
         data_frame = self.clean_datetime_columns(data_frame, 'CRSDepTime')
         data_frame = self.clean_datetime_columns(data_frame, 'CRSArrTime')
@@ -16,6 +37,13 @@ class FlightDataManager:
         DataManager().write_data(data_frame, output_path, output_format, write_mode)
 
     def clean_datetime_columns(self, data_frame, column):
+        """
+        Method concatenates Year, Month and DayOfMonth columns together and adds ':' to the time columns which was a
+        float and is concatenated to the above columns. The final value is casted as a timestamp.
+        :param data_frame: Spark data frame instance with the input data to be transfomed.
+        :param column: Column on which the transformation needs to be applied.
+        :return: Spark data frame instance with the transformation applied to the given column.
+        """
         return data_frame.withColumn('Date', concat_ws('-', 'Year', 'Month', 'DayOfMonth')) \
             .withColumn('Time', lpad(data_frame[column], 4, '0')) \
             .withColumn("Time", regexp_replace(col("Time"), "(\\d{2})(\\d{2})", "$1:$2")) \
@@ -23,11 +51,36 @@ class FlightDataManager:
             .withColumn(column, to_timestamp(column, 'yyyy-MM-dd HH:mm'))
 
     def calculate_actual_time(self, data_frame, column, delay, rename_column):
+        """
+        Method calculates actual departure and arrival time. DepTime and ArrTime columns are not used since there is
+        chance corruption of data close to midnight, since it contains only the time part. So instead the DepDelay or
+        ArrDelay value is added to the scheduled departure and scheduled arrival columns respectively to get the actual
+        departure and arrival timestamp.
+        :param data_frame: Spark dataframe whose actual times need to be calculated.
+        :param column: Column containing scheduled value for departure or arrival.
+        :param delay: Column containing the delays for departure or arrival.
+        :param rename_column: Column to which the data needs to be stored.
+        :return: Spark data frame with the update the timestamps.
+        """
         return data_frame.withColumn(rename_column,
                                      to_timestamp(from_unixtime(unix_timestamp(column) + (col(delay) * 60))))
 
     def enhance_flight_details(self, spark, flight_path, aircraft_path, airport_path, carrier_path, output_path,
                                output_format, write_mode='error'):
+        """
+        Method enhances the flight information with aircraft, airport and carrier data and store it in output folder in
+        the specified format.
+        :param spark: SparkSession instance.
+        :param flight_path: Path to clean flight data.
+        :param aircraft_path: Path to aircraft data.
+        :param airport_path:  Path to airport data.
+        :param carrier_path: Path to carrier data.
+        :param output_path: Path where enhanced flight data to be stored.
+        :param output_format: Format in which the enhanced flight data to be stored.
+        :param write_mode: File write mode with default to 'error', which throws an error, if the output folder is not
+        empty.
+        :return: NoneType.
+        """
         data_manager = DataManager()
         flights = data_manager.read_data(spark, flight_path, 'parquet') \
             .withColumn('FlightNum', concat('UniqueCarrier', 'FlightNum'))
@@ -38,6 +91,7 @@ class FlightDataManager:
         airport = data_manager.read_data(spark, airport_path, 'parquet')
         carrier = data_manager.read_data(spark, carrier_path, 'parquet')
 
+        # Joining Flight data with aircraft data on tailnum.
         flights = flights.join(aircraft, 'tailnum', 'left_outer') \
             .withColumnRenamed('manufacturer', 'AircraftManufacturer') \
             .withColumnRenamed('issue_date', 'AircraftIssueDate') \
@@ -45,6 +99,7 @@ class FlightDataManager:
             .withColumnRenamed('aircraft_type', 'AircraftType') \
             .withColumnRenamed('engine_type', 'EngineType')
 
+        # Joining flight data with airport data for origin airport information.
         flights = flights.join(airport, flights['Origin'] == airport['iata'], 'left_outer') \
             .withColumnRenamed('Origin', 'OriginIATACode') \
             .withColumnRenamed('airport', 'OriginAirport') \
@@ -55,6 +110,7 @@ class FlightDataManager:
             .withColumnRenamed('long', 'OriginLongitude') \
             .drop('iata')
 
+        # Joining flight data with airport data for destination information.
         flights = flights.join(airport, flights['Dest'] == airport['iata'], 'left_outer') \
             .withColumnRenamed('Dest', 'DestinationIATACode') \
             .withColumnRenamed('airport', 'DestinationAirport') \
@@ -65,6 +121,7 @@ class FlightDataManager:
             .withColumnRenamed('long', 'DestinationLongitude') \
             .drop('iata')
 
+        # Joining flight data with carrier information on carrier code.
         flights = flights.join(carrier, flights['UniqueCarrier'] == carrier['code'], 'left_outer') \
             .withColumnRenamed('UniqueCarrier', 'CarrierCode').withColumnRenamed('Description', 'Carrier').drop('code')
 
@@ -72,7 +129,22 @@ class FlightDataManager:
 
 
 class AircraftManager:
+    """
+    Class contains all the transformation and data cleaning methods for aircraft data.
+    """
+
     def clean_aircraft_data(self, spark, path, format, output_path, output_format, write_mode='error'):
+        """
+        Method keeps only the aircraft whose 'status' column whose value equals 'Valid'.
+        :param spark: SparkSessin instance.
+        :param path: Path to aircraft data.
+        :param format: Format in which the aircraft data is stored.
+        :param output_path: Output path the where the clean aircraft to be stored.
+        :param output_format: Output format in which the clean aircraft data to be stored.
+        :param write_mode: File write mode with default to 'error', which throws an error, if the output folder is not
+        empty.
+        :return: NoneType.
+        """
         data_frame = DataManager().read_data(spark, path, format)
         data_frame = data_frame.filter("status == 'Valid'")
         DataManager().write_data(data_frame, output_path, output_format, write_mode)
